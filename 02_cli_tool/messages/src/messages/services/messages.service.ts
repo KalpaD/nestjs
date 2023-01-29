622a5dee679d96f20730041d7e3b063007c4f9c1
Kalpa Senanayake <senanayake.kalpa@gmail.com>
2023-01-30 09:10:57 +1100

    02_cli_tool: Adding generated project with basic implementation

---
+++ 02_cli_tool/messages/src/messages/services/messages.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { MessageRepository } from "../repositories/messages.repository";
+
+@Injectable()
+export class MessageService {
+    constructor(public messagesRepo: MessageRepository) {
+    
+    }
+
+    findOne(id: string) {
+        return this.messagesRepo.findOne(id);
+    }
+
+    findAll() {
+        return this.messagesRepo.findAll();
+    }
+
+    create(content: string) {
+        return this.messagesRepo.create(content);
+    }
+}
